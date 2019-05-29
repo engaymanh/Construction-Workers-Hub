7ee1c6d591f828de8f93f5eae16872935677bf9f
engaymanh <eng.aymanhariri@gmail.com>
2019-05-29 13:34:38 +0300

    start css

--- client/index.js
+++ client/index.js
@@ -1,39 +1,39 @@
-var app = angular.module('app', [ 'ngRoute' ]);
+var app = angular.module("app", ["ngRoute", "firebase"]);
 app.config(function($routeProvider) {
-	$routeProvider
-		.when('/', {
-			templateUrl: './compnent/homehtml.html',
-			controller: 'testctrl'
-		})
-		.when('/signup', {
-			templateUrl: './compnent/signup.html',
-			controller: 'homeController'
-		})
-		.when('/workerSignup', {
-			templateUrl: './compnent/workerSignup.html',
-			controller: 'workerSignup'
-		})
-		.when('/workerSignin', {
-			templateUrl: './compnent/workerSignin.html',
-			controller: 'workerSignin'
-		})
-		.when('/signupEng', {
-			templateUrl: './compnent/signupEng.html',
-			controller: 'signupEng'
-		})
-		.when('/WorkerProfile/:id', {
-			templateUrl: './compnent/WorkerProfile.html',
-			controller: 'WorkerProfile'
-		})
-		.when('/workerscategory', {
-			templateUrl: './compnent/workerscategory.html',
-			controller: 'workerscategory'
-		})
-		.when('/workerPage', {
-			templateUrl: './compnent/workerMainPage.html',
-			controller: 'workerMainPageController'
-		});
+  $routeProvider
+    .when("/", {
+      templateUrl: "./compnent/homehtml.html",
+      controller: "testctrl"
+    })
+    .when("/signup", {
+      templateUrl: "./compnent/signup.html",
+      controller: "homeController"
+    })
+    .when("/workerSignup", {
+      templateUrl: "./compnent/workerSignup.html",
+      controller: "workerSignup"
+    })
+    .when("/workerSignin", {
+      templateUrl: "./compnent/workerSignin.html",
+      controller: "workerSignin"
+    })
+    .when("/signupEng", {
+      templateUrl: "./compnent/signupEng.html",
+      controller: "signupEng"
+    })
+    .when("/WorkerProfile/:id", {
+      templateUrl: "./compnent/WorkerProfile.html",
+      controller: "WorkerProfile"
+    })
+    .when("/workerscategory", {
+      templateUrl: "./compnent/workerscategory.html",
+      controller: "workerscategory"
+    })
+    .when("/workerPage", {
+      templateUrl: "./compnent/workerMainPage.html",
+      controller: "workerMainPageController"
+    });
 });
-app.service('worker_id_service', function() {
-	var worker_id;
+app.service("worker_id_service", function() {
+  var worker_id;
 });
