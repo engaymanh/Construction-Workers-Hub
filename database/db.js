796c2a6853cf591fcd427d3e1af917fe49e3065c
engaymanh <eng.aymanhariri@gmail.com>
2019-05-30 00:23:09 +0300

    template signup worker conecting

--- database/db.js
+++ database/db.js
@@ -1,14 +1,14 @@
-var mysql = require('mysql');
-var Sequelize = require('sequelize');
+var mysql = require("mysql");
+var Sequelize = require("sequelize");
 
 //Database connection
-const db = new Sequelize('sql2293815', 'sql2293815', 'tL4!hH5!', {
-	host: 'sql2.freesqldatabase.com',
-	dialect: 'mysql'
+const db = new Sequelize("sql7293890", "sql7293890", "pcPNdQLGzs", {
+  host: "sql7.freesqldatabase.com",
+  dialect: "mysql"
 });
 
 db.sync({ force: false, logging: false }).then(() => {
-	console.log(`Database & tables created!`);
+  console.log(`Database & tables created!`);
 });
 
 module.exports.db = db;
