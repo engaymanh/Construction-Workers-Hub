38911d84a34a54b93f5b6405fd9e4f3b59308968
engaymanh <eng.aymanhariri@gmail.com>
2019-05-30 14:52:41 +0300

    conflict

--- client/compnent/workerMainPageController.js
+++ client/compnent/workerMainPageController.js
@@ -1,40 +1,46 @@
-app.controller('workerMainPageController', function($scope, $http, $log, $location) {
-	$scope.engName = 'hi';
-	$scope.fullName = '';
-	$scope.expectedSalary = '';
-	$scope.experienceLevel = '';
-	$scope.phoneNumber = '';
-	$scope.url = 'https://cdn0.iconfinder.com/data/icons/flat-vector-2/100/77-Under_Construction-512.png';
-	const token = localStorage.getItem('token');
-	//   const successCallBackEngName = response => {
-	//     $scope.engName = response.data.engineerName;
-	//     $log.info(response);
-	//   };
-	const successCallBackWorkerPage = (response) => {
-		$scope.phoneNumber = response.data.phoneNumber;
-		$scope.fullName = response.data.fullName;
-		$scope.experienceLevel = response.data.experienceLevel;
-		$scope.expectedSalary = response.data.expectedSalary;
-		$scope.url =
-			response.data.url ||
-			'https://cdn0.iconfinder.com/data/icons/flat-vector-2/100/77-Under_Construction-512.png';
-		$log.info(response);
-	};
-	const errorCallBack = (response) => {
-		$scope.error = response.data;
-	};
-	const workerPage = () => {
-		$http({
-			method: 'GET',
-			headers: {
-				'x-access-token': token
-			},
-			url: '/workerPage'
-		}).then(successCallBackWorkerPage, errorCallBack);
-		// $http('/workerPage',).then(successCallBack,errorCallBack)
-	};
+app.controller("workerMainPageController", function(
+  $scope,
+  $http,
+  $log,
+  $location
+) {
+  $scope.engName = "hi";
+  $scope.fullName = "";
+  $scope.expectedSalary = "";
+  $scope.experienceLevel = "";
+  $scope.phoneNumber = "";
+  $scope.url =
+    "https://cdn0.iconfinder.com/data/icons/flat-vector-2/100/77-Under_Construction-512.png";
+  const token = localStorage.getItem("token");
+  //   const successCallBackEngName = response => {
+  //     $scope.engName = response.data.engineerName;
+  //     $log.info(response);
+  //   };
+  const successCallBackWorkerPage = response => {
+    $scope.phoneNumber = response.data.phoneNumber;
+    $scope.fullName = response.data.fullName;
+    $scope.experienceLevel = response.data.experienceLevel;
+    $scope.expectedSalary = response.data.expectedSalary;
+    $scope.url =
+      response.data.url ||
+      "https://cdn0.iconfinder.com/data/icons/flat-vector-2/100/77-Under_Construction-512.png";
+    $log.info(response);
+  };
+  const errorCallBack = response => {
+    $scope.error = response.data;
+  };
+  const workerPage = () => {
+    $http({
+      method: "GET",
+      headers: {
+        "x-access-token": token
+      },
+      url: "/workerPage"
+    }).then(successCallBackWorkerPage, errorCallBack);
+    // $http('/workerPage',).then(successCallBack,errorCallBack)
+  };
 
-	workerPage();
+  workerPage();
 });
 
 // app.controller('workerMainPageController', function($scope, $http, $log) {
